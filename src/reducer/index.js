88a656a3075129f9137f3d64566d8d3cb9c240e2
MobileDev418 <oncemoreforyou@outlook.com>
2018-11-27 09:40:54 -0800

    added reduce

---
+++ src/reducer/index.js
@@ -0,0 +1,23 @@
+import { combineReducers, createStore, compose , applyMiddleware} from "redux";
+import  thunk from 'redux-thunk'
+import  createLogger from 'redux-logger'
+import authService from "../modules/AuthService";
+import tabService from "../modules/TabService";
+import notiService from "../modules/NotiService";
+import navService from "../modules/NavService";
+
+const Reducer = combineReducers({
+  authService,
+  tabService,
+  notiService,
+  navService
+});
+const rootReducer = (state, action) => {
+	return Reducer(state,action);
+}
+
+const logger = createLogger();
+
+let store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+
+export default store;
