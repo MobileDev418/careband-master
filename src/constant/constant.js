2f915b723277e36a3902cdef0958f5d10b803a53
MobileDev418 <oncemoreforyou@outlook.com>
2018-11-28 15:42:00 +0800

    added initial constant

---
+++ src/constant/constant.js
@@ -0,0 +1,75 @@
+ const MBLUE = '#3eb8be';
+ const MTAB = '#d7d8d8';
+ const MALERT = '#c22e5a';
+ const MGREEN = '#4bad52';
+ const MYELLOW = '#f4b669';
+ const MTEXT = '#334954';
+ const MGREY = '#b4b4b4';
+ const MBORDERGREY = '#e7e7e7';
+
+ const TITLEFSIZE = 24;
+ const TABFSIZE = 15;
+ const CONTENTFSIZE = 17;
+ const SUBCONTENTFSIZE = 15;
+ const TIMEFSIZE = 13;
+ const STATEFSIZE = 12;
+
+ const NAVHEIGHT = 74;
+ const TABHEIGHT = 44;
+ const PHOTOSIZE = 55;
+ const ICONSIZE = 50;
+ const LOCATESIZE = 45;
+ const NOTESIZE = 50;
+ const ACTIVESIZE = 35;
+ const PROFILEPHOTOSIZE = 120;
+
+ const USERID = "USERID";
+ const ACCESSTOKEN = "ACCESSTOKEN";
+
+ const user = {
+    _id: "09CEE95A-ED9B-CE04-6618-207F1E8404E0",
+    firstname: "Phillip",
+    lastname: "Tanek",
+    email: "test@gmail.com",
+    password: "123456",
+    photo: "https://randomuser.me/api/portraits/men/4.jpg"
+};
+
+ const strings = {
+     USERID,
+     ACCESSTOKEN
+ }
+ const colors = {
+    MBLUE,
+    MBLUE,
+    MTAB,
+    MALERT,
+    MGREEN,
+    MYELLOW,
+    MTEXT,
+    MGREY,
+    MBORDERGREY
+ }
+
+ const fonts = {
+     TITLEFSIZE,
+     TABFSIZE,
+     CONTENTFSIZE,
+     SUBCONTENTFSIZE,
+     STATEFSIZE,
+     TIMEFSIZE
+ }
+
+ const dimens = {
+    NAVHEIGHT,
+    TABHEIGHT,
+    PHOTOSIZE,
+    PROFILEPHOTOSIZE,
+    ICONSIZE,
+    LOCATESIZE,
+    NOTESIZE,
+    ACTIVESIZE
+ }
+ export {
+    colors, fonts, dimens, strings, user
+ }
